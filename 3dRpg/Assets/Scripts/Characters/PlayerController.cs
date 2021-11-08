b68685df27d3980417396377a3cdcc49ebcf0a91
l0velyr1ver <1138822516@qq.com>
2021-11-08 19:40:37 +0800

    Animator 动画控制器

--- 3dRpg/Assets/Scripts/Characters/PlayerController.cs
+++ 3dRpg/Assets/Scripts/Characters/PlayerController.cs
@@ -6,15 +6,26 @@ using UnityEngine.AI;
 public class PlayerController : MonoBehaviour
 {
     private NavMeshAgent agent;
-
+    private Animator anim;
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
     }
 
     private void Start()
     {
         MouseManager.Instance.OnMouseClicked += MoveToTarget;
+    }
+
+    private void Update()
+    {
+        SwitchAnimation();
+    }
+
+    private void SwitchAnimation()
+    {
+        anim.SetFloat("Speed", agent.velocity.sqrMagnitude);
     }
 
     public void MoveToTarget(Vector3 target)
