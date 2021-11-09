f4e5b03f1124efd739d43f8aa45a11fbd9cfe575
l0velyr1ver <1138822516@qq.com>
2021-11-09 11:35:23 +0800

    Enemy Set States 设置敌人的基本属性和状态 导入敌人素材 创建 EnemyController 脚本并通过脚本添加 Agent 设置 Agent 参数 枚举变量设置敌人状态 修改鼠标指针到攻击图标

---
+++ 3dRpg/Assets/Scripts/Characters/EnemyController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public enum EnemyStates { GUARD,PATROL,CHASE,DEAD}
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyController : MonoBehaviour
+{
+    public EnemyStates enemyStates;
+    private NavMeshAgent agent;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        SwitchStates();
+    }
+
+    void SwitchStates()
+    {
+        switch (enemyStates)
+        {
+            case EnemyStates.GUARD:
+                break;
+            case EnemyStates.PATROL:
+                break;
+            case EnemyStates.CHASE:
+                break;
+            case EnemyStates.DEAD:
+                break;
+        }
+    }
+
+}
--- 3dRpg/Assets/Scripts/Managers/MouseManager.cs
+++ 3dRpg/Assets/Scripts/Managers/MouseManager.cs
@@ -41,6 +41,9 @@ public class MouseManager : MonoBehaviour
                 case "Ground":
                     Cursor.SetCursor(target,new Vector2(16,16),CursorMode.Auto);
                     break;
+                case "Enemy":
+                    Cursor.SetCursor(attack, new Vector2(16, 16), CursorMode.Auto);
+                    break;
             }
         }
     }
